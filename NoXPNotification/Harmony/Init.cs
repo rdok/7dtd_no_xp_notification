897fffd9fbbf1b8b5cbf3ae447c1bd8b2f548f8f
Rizart Dokollari <r.dokollari@gmail.com>
2024-08-14 11:22:37 +0100

    chore: Initialise project.

---
+++ NoXPNotification/Harmony/Init.cs
@@ -0,0 +1,13 @@
+﻿namespace NoXPNotification.Harmony
+{
+    public class Init : IModApi
+    {
+        private static readonly ILogger Logger = new Logger();
+
+        public void InitMod(Mod modInstance)
+        {
+            var harmony = new HarmonyLib.Harmony("uk.co.rdok.7daystodie.mod.no_xp_notification");
+            harmony.PatchAll();
+        }
+    }
+}
